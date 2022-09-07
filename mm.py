47d7b846d13e379611b35d8ce20df0a1e5f086f5
mlfisch3 <38837568+mlfisch3@users.noreply.github.com>
2022-09-07 02:07:44 -0400

    fix indent

--- mm.py
+++ mm.py
@@ -2,52 +2,52 @@ import weakref
 
 
 def to_dead(wref):
-    '''
-    returns true if object referred to by weak reference is dead
-    '''
-    #len([u for u in str(_11).replace('>',' ').split(' ') if u == 'dead'])
-    return any([u=='dead' for u in str(wref).replace('>',' ').split(' ')])
+  '''
+  returns true if object referred to by weak reference is dead
+  '''
+  #len([u for u in str(_11).replace('>',' ').split(' ') if u == 'dead'])
+  return any([u=='dead' for u in str(wref).replace('>',' ').split(' ')])
 
 
 def get_mmaps():
-     '''
-     gets filename for each memory-mapped variable
-    Usage
-    >>> mmaps = get_mmaps()
-    >>> for k,v in mmaps.items():
-          print(k,v)
-    '''
-    mempaths={}
-    globals_copy = globals().copy()
-    for name in globals_copy.keys():
-        try:
-            name_ref = weakref.ref(eval(compile(name,'tmp.txt', 'eval')))
-        except (NameError, TypeError):
-            continue
-        if hasattr(name_ref(), '__dict__'):
-            v = vars(name_ref())
-            if '_mmap' in v.keys():
-                mempaths[name] = v['filename']
+  '''
+  gets filename for each memory-mapped variable
+  Usage
+  >>> mmaps = get_mmaps()
+  >>> for k,v in mmaps.items():
+        print(k,v)
+  '''
+  mempaths={}
+  globals_copy = globals().copy()
+  for name in globals_copy.keys():
+      try:
+          name_ref = weakref.ref(eval(compile(name,'tmp.txt', 'eval')))
+      except (NameError, TypeError):
+          continue
+      if hasattr(name_ref(), '__dict__'):
+          v = vars(name_ref())
+          if '_mmap' in v.keys():
+              mempaths[name] = v['filename']
 
-    return mempaths
+  return mempaths
 
 
 
 def get_weakrefs():
-    '''
-    Usage
-    >>> weakrefs = get_weakrefs()
-    >>> for k,v in weakrefs.items():
-          print(k,v)
-    '''
+  '''
+  Usage
+  >>> weakrefs = get_weakrefs()
+  >>> for k,v in weakrefs.items():
+        print(k,v)
+  '''
 
-    weak={}
+  weak={}
 
-    globals_copy = globals().copy()
-    for f in globals_copy.keys():
-        if type(globals_copy[f])==weakref.ref:
-            weak[f]=globals_copy[f]
-    return weak
+  globals_copy = globals().copy()
+  for f in globals_copy.keys():
+      if type(globals_copy[f])==weakref.ref:
+          weak[f]=globals_copy[f]
+  return weak
 
 
 if __name__ == '__main__':
